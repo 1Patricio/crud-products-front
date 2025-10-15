2546c737a72c6249a15f1dc1059bff4737d4c42e
Anderson Patricio <andersongpatricio@hotmail.com>
2025-10-15 08:27:36 -0300

    fix: ajustando rotas ambiente de produção

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import { copyFileSync } from 'fs'
 
 export default defineConfig({
   plugins: [
@@ -9,6 +10,12 @@ export default defineConfig({
     }),
     quasar({
       sassVariables: 'src/quasar-variables.sass'
-    })
+    }),
+    {
+      name: 'copy-redirects',
+      closeBundle() {
+        copyFileSync('_redirects', 'dist/_redirects')
+      }
+    }
   ]
 })
