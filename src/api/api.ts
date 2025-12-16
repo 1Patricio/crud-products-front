7fc1c4cb720849380ecf6a6a071606f7b029cb2b
Anderson Patricio <andersongpatricio@hotmail.com>
2025-12-16 08:00:11 -0300

    feat: ajustando baseURL para verificar se o ambiente é de produção

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const isLocalhost = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+
 const api = axios.create({
-    baseURL: "https://crud-products-back.onrender.com/",
+    baseURL: isLocalhost ? "http://localhost:8081/" : "https://crud-products-back.onrender.com/",
     headers: {
         "Content-Type": "application/json"
     }
